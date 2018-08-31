3edfbf33147e0af34e4064221cdea1d970b258a1
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-31 17:09:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function loadIssue() {
+  let issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  };
+  
   
 }
